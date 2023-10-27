03d7fd3ae16137ebcf9fc76e11938144563b162f
Elul Asfaha <scorpiondota22@gmail.com>
2023-10-27 21:18:35 +0300

    cryptocurrency app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const App = () => {
+    return (
+        <div>
+            <div>nav</div>
+            <div>main</div>
+            <div>footer</div>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+const NavBar = () => {
+    return (
+        <div>
+            <div>
+                <div>Avatar</div>
+                <div>
+                    logo
+                    <Link to=''>Cryptoverse</Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NavBar;
