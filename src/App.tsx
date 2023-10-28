b86808faad1cbbf10f09612980defd2792aa6413
Elul Asfaha <scorpiondota22@gmail.com>
2023-10-28 18:37:23 +0300

    using basic fetch for now set up dummy data but works with api request trying not to reach api limit

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,49 @@ import {
     CryptoDetails,
     Footer,
 } from "./components";
+import { useEffect, useState } from "react";
 const App = () => {
+    const [stats, setStats] = useState({
+        total: 616,
+        total24hVolume: "138577035327",
+        totalCoins: 31156,
+        totalMarkets: 36493,
+        totalExchanges: 164,
+        totalMarketCap: "1273225457529",
+    });
+    // const fetchdata = async () => {
+    //     const url =
+    //         "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0";
+    //     const options = {
+    //         method: "GET",
+    //         headers: {
+    //             "X-RapidAPI-Key":
+    //                 "d1fd8aa275mshdfbdece75472e69p13a6b7jsnec1cb651fbf3",
+    //             "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+    //         },
+    //     };
+
+    //     try {
+    //         const response = await fetch(url, options);
+    //         const result = await response.json();
+    //         setStats(result.data.stats);
+    //     } catch (error) {
+    //         console.error(error);
+    //     }
+    // };
+    // useEffect(() => {
+    //     fetchdata();
+    // }, []);
     return (
         <div className='h-screen w-full'>
             <div className='flex h-full'>
                 <NavBar />
                 <div className='w-full'>
                     <Routes>
-                        <Route path='/' element={<Home />} />
+                        <Route
+                            path='/'
+                            element={<Home displayStatus={stats} />}
+                        />
                         <Route path='/exchange' element={<Exchanges />} />
                         <Route
                             path='/cryptocurrencies'
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,61 +1,39 @@
 import millify from "millify";
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
-const Home = () => {
-    const fetchdata = async () => {
-        const url =
-            "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0";
-        const options = {
-            method: "GET",
-            headers: {
-                "X-RapidAPI-Key":
-                    "d1fd8aa275mshdfbdece75472e69p13a6b7jsnec1cb651fbf3",
-                "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
-            },
-        };
-
-        try {
-            const response = await fetch(url, options);
-            const result = await response.text();
-            console.log(result);
-        } catch (error) {
-            console.error(error);
-        }
+type HomeType = {
+    displayStatus?: {
+        total?: number;
+        total24hVolume?: string;
+        totalCoins?: number;
+        totalExchanges?: number;
+        totalMarketCap?: string;
+        totalMarkets?: number;
     };
-    useEffect(() => {
-        // fetchdata();
-    }, []);
+};
+const Home = (displayStatus: HomeType) => {
     return (
         <div className='bg-gray-50 p-5'>
             <p className='text-3xl py-10'>Global Crypto Stats</p>
             <div className='grid grid-cols-2 gap-y-3'>
                 <div className='flex flex-col gap-1'>
                     <p className=' text-gray-500'>Total CryptoCurrencies</p>
-                    <p>5</p>
+                    <p>{displayStatus.displayStatus?.total}</p>
                 </div>
                 <div className='flex flex-col gap-1'>
-                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
-                    <p>5</p>
+                    <p className=' text-gray-500'>Total Exchanges</p>
+                    <p>{displayStatus.displayStatus?.totalExchanges}</p>
                 </div>
                 <div className='flex flex-col gap-1'>
-                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
-                    <p>5</p>
+                    <p className=' text-gray-500'>Total Market Cap</p>
+                    <p>{displayStatus.displayStatus?.totalMarketCap}</p>
                 </div>
                 <div className='flex flex-col gap-1'>
-                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
-                    <p>5</p>
+                    <p className=' text-gray-500'>Total 24h Volume</p>
+                    <p>{displayStatus.displayStatus?.total24hVolume}</p>
                 </div>
                 <div className='flex flex-col gap-1'>
-                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
-                    <p>5</p>
-                </div>
-                <div className='flex flex-col gap-1'>
-                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
-                    <p>5</p>
-                </div>
-                <div className='flex flex-col gap-1'>
-                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
-                    <p>5</p>
+                    <p className=' text-gray-500'>Total Markets</p>
+                    <p>{displayStatus.displayStatus?.totalMarkets}</p>
                 </div>
             </div>
         </div>
