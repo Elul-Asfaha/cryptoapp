95ef6e4d08a511053e10b25f89ef08f4118542de
Elul Asfaha <scorpiondota22@gmail.com>
2023-10-28 15:23:31 +0300

    added reduxtoolkit created a store and api request from rapidapi

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,8 +2,8 @@ import { Link } from "react-router-dom";
 const Footer = () => {
     return (
         <div className='flex flex-col item-center justify-center text-lg gap-1 p-3 bg-blue-950 text-blue-500'>
-            <p className='text-white text-center'>Cryptoverse</p>
-            <p className='text-white text-center text-sm'>
+            <p className='text-white text-center font-bold'>Cryptoverse</p>
+            <p className='text-white text-center font-semibold text-sm'>
                 All rights reserved
             </p>
             <div className='flex gap-5 justify-center'>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,8 +2,38 @@ import millify from "millify";
 import { Link } from "react-router-dom";
 const Home = () => {
     return (
-        <div className=''>
-            <p className='text-3xl'>Global Crypto Stats</p>
+        <div className='bg-gray-50 p-5'>
+            <p className='text-3xl py-10'>Global Crypto Stats</p>
+            <div className='grid grid-cols-2 gap-y-3'>
+                <div className='flex flex-col gap-1'>
+                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
+                    <p>5</p>
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
+                    <p>5</p>
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
+                    <p>5</p>
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
+                    <p>5</p>
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
+                    <p>5</p>
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
+                    <p>5</p>
+                </div>
+                <div className='flex flex-col gap-1'>
+                    <p className=' text-gray-500'>Total CryptoCurrencies</p>
+                    <p>5</p>
+                </div>
+            </div>
         </div>
     );
 };
